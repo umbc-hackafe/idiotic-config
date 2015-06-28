ecb4d3b987f6985184d90d89350ad6e83df4f82b
Dylan Whichard <dylan@whichard.com>
2015-06-28 17:14:25 -0400

    Add some rules

---
+++ rules/media.py
@@ -0,0 +1,27 @@
+from idiotic.rule import bind, Command, Change, Schedule, augment, Delay, DeDup
+from idiotic.scene import Scene
+from idiotic import items, scheduler, modules, scenes
+
+class LivingRoomMedia(Scene):
+    control = (items.living_room_lamp, items.kitchen_table_light)
+    def entered(self):
+        for item in self.control:
+            item.off()
+            item.disable()
+
+    def exited(self):
+        for item in self.control:
+            item.enable()
+            item.on()
+
+class GarageMedia(Scene):
+    control = ((items.garage_lights, ("off", "on")),
+               (items.garage_projector_screen, ("forward", "reverse")))
+
+    def entered(self):
+        for item, (act, _) in self.control:
+            item.command(act)
+
+    def exited(self):
+        for item, (_, act) in self.control:
+            item.command(act)
---
+++ rules/motion.py
@@ -0,0 +1,47 @@
+from idiotic.rule import bind, Command, Change, Schedule, augment, Delay, DeDup
+from idiotic import items, scheduler, modules, scenes
+
+class Daylight(Scene):
+    def entered(self):
+        for item in items.with_tag('nightlight'):
+            item.off()
+            item.disable()
+
+    def exited(self):
+        for item in items.with_tag('nightlight'):
+            item.enable()
+            item.on()
+
+mapping = [(items.kitchen_motion, items.kitchen_light),
+           (items.living_room_motion, items.living_room_lamp),
+           (items.hall_motion, items.hall_light),
+           (items.laundry_room_motion, items.laundry_room_light)]
+
+# This might not actually work with triggers rather than toggles for sensors
+# at the very least, it's not very elegant otherwise...
+for sensor, light in mapping:
+    @bind(Command(sensor))
+    @augment(Delay(Command(sensor, "OFF"), period=300,
+             cancel=Command(sensor, "ON")))
+    def rule(evt):
+        light.command(evt.command)
+
+@bind(Command(items.bathroom_door))
+@augment(Delay(Command(items.bathroom_door, "OFF"),
+               period=10,
+               cancel=Command(items.bathroom_door, "ON"),
+               consume=Command(items.bathroom_door, "OFF")))
+def bathroom_rule(evt):
+    if evt.new:
+        bathroom_light.on()
+    else:
+        hall_motion.trigger()
+        bathroom_light.off()
+
+@bind(Schedule(scheduler.every().day.at("8:00")))
+def enter_sun_mode(evt):
+    scenes.daylight.enter()
+
+@bind(Schedule(scheduler.every().day.at("19:00")))
+def leave_sun_mode(evt):
+    scenes.daylight.exit()
