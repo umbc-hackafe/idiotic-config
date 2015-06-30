60418313966d7d954f1dd8b488a735e9c9de9fbb
Dylan Whichard <dylan@whichard.com>
2015-06-30 00:15:00 -0400

    Fix garage stuff

--- items/garage.py
+++ items/garage.py
@@ -1,15 +1,23 @@
-from idiotic.item import Toggle, Trigger, Number, Group
+from idiotic.item import Toggle, Trigger, Number, Group, Motor
 from idiotic import modules, scheduler
 
 Toggle("Garage Lights",
        bindings={"x10": {"code": "c2"}},
        tags=("garage", "light"))
 
-Trigger("Garage Door Opener",
-        tags=("garage", "external_door"))
+Toggle("Garage Door Opener",
+       bindings={"http":
+                 {"on": "thegreatandpowerfultrixie:8081/door?action=trigger",
+                  "off": "thegreatandpowerfultrixie:8081/door?action=close"}},
+       tags=("garage", "external_door"))
 
 Toggle("Garage Door",
         tags=("garage", "external_door"))
 
-Toggle("Garage Projector Screen",
-       tags=("garage",))
+Motor("Garage Projector Screen",
+      constrained=True,
+      bindings={"http":
+                {"forward": "thegreatandpowerfultrixie:8081/projector?action=down",
+                 "reverse": "thegreatandpowerfultrixie:8081/projector?action=up",
+                 "stop": "thegreatandpowerfultrixie:8081/projector?action=stop"}},
+      tags=("garage",))
