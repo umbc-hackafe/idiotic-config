110dd30d1a6e53ef649a78e912f71b84a55f4f9e
Dylan Whichard <dylan@whichard.com>
2015-07-04 23:09:00 -0400

    Add brightness meter

---
+++ items/weather.py
@@ -0,0 +1,6 @@
+from idiotic.item import Toggle, Trigger, Number, Group
+from idiotic import modules, scheduler
+
+Number("Brightness",
+       bindings={"http": {"pull": "luna:8081/lx"}},
+       tags=("weather", "climate"))
