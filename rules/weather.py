c7279514dcf5b9620d9af3b51ccbcc84730eb55a
Dylan Whichard <dylan@whichard.com>
2015-07-06 20:45:30 -0400

    Trigger daylight scene with brightness

---
+++ rules/weather.py
@@ -0,0 +1,15 @@
+from idiotic.rule import bind, Command, Change, Schedule, augment, Delay, DeDup
+from idiotic import items, scheduler, modules, scenes
+
+MIN_DAY_BRIGTHNESS = 15
+MAX_NIGHT_BRIGHTNESS = 5
+
+@bind(Change(items.brightness))
+def brightness_change(evt):
+    if evt.old is None:
+        continue
+
+    if evt.old < MIN_DAY_BRIGHTNESS < evt.new:
+        scenes.daylight.enter()
+    elif evt.old > MAX_NIGHT_BRIGHTNESS > evt.new:
+        scenes.daylight.exit()
