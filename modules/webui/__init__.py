e3f9b5621631dde66998b7316e2dffdb4db9d3c5
Dylan Whichard <dylan@whichard.com>
2015-08-03 15:06:36 -0400

    Add support for sections in webui

--- modules/webui/__init__.py
+++ modules/webui/__init__.py
@@ -8,10 +8,16 @@ import requests
 import functools
 from idiotic import dispatcher, event, items, scenes, utils
 from idiotic.item import Toggle, Trigger, Number, Motor
+from idiotic.scene import Scene
 
 MODULE_NAME = "webui"
 
-log = logging.getLogger("module.webui")
+LOG = logging.getLogger("module.webui")
+
+SECT_INCLUDE = ("include_tags",
+                "exclude_tags",
+                "include_items",
+                "exclude_items")
 
 include_tags = set()
 exclude_tags = set()
@@ -27,35 +33,31 @@ def configure(config, api, assets):
     template_args["title"] = config.get("page_title", "idiotic")
     template_args["root"] = api.path
 
-    include_tags = set(config.get("include_tags", []))
-    exclude_tags = set(config.get("exclude_tags", []))
-    include_items = set(config.get("include_items", []))
-    exclude_items = set(config.get("exclude_items", []))
     asset_path = assets
-
     env = jinja2.Environment(loader=jinja2.FileSystemLoader(asset_path))
 
-    api.serve(functools.partial(_main_page, include_tags, exclude_tags,
-                                include_items, exclude_items),
-              '/', content_type="text/html")
     api.serve(_main_js, '/main.js', content_type="text/javascript")
-    traverse(api, '/', config.get("subpages", {}))
+
+    traverse(api, "/", config)
+
+    return
 
 def traverse(api, path, tree):
     if not tree:
         return
 
-    for key, val in tree.items():
-        include_tags = set(val.get("include_tags", []))
-        exclude_tags = set(val.get("exclude_tags", []))
-        include_items = set(val.get("include_items", []))
-        exclude_items = set(val.get("exclude_items", []))
+    sections = []
+    if "sections" in tree:
+        sections = tree["sections"]
+    else:
+        sections = [{k: tree[k] for k in tree
+                     if k in SECT_INCLUDE }]
 
-        api.serve(functools.partial(_main_page, include_tags,
-                                    exclude_tags, include_items, exclude_items),
-                  utils.join_url(path, key),
-                  content_type="text/html")
-        traverse(api, utils.join_url(path, key), val.get("subpages", {}))
+    api.serve(functools.partial(_main_page, sections),
+              path, content_type="text/html")
+
+    for key, val in tree.get("subpages", {}):
+        traverse(api, utils.join_url(path, key), val)
 
 def scene(name, command=None, **kwargs):
     scene = getattr(scenes, name)
@@ -68,52 +70,57 @@ def scene(name, command=None, **kwargs):
             command, scene))
     return scene.active
 
-def _main_page(include_tags, exclude_tags, include_items, exclude_items, *_, **__):
+def _main_page(sections, *_, **__):
     args = dict(template_args)
-    args["items"] = []
-    for item in sorted(items.all(), key=lambda i:i.name):
-        if not _include_item(item, include_tags, exclude_tags,
-                             include_items, exclude_items):
-            continue
+    args["sections"] = []
+    for section in sections:
+        include_tags, exclude_tags, include_items, exclude_items = (
+            set(section.get(n, [])) for n in SECT_INCLUDE
+        )
+        sect = {"item_list": []}
+        sect.update({k: v for k, v in section.items()
+                     if k not in SECT_INCLUDE})
 
-        item_dict = {
-            "desc": item.name,
-            "name": utils.mangle_name(item.name),
-            "show_disable": "webui.show_disable" in item.tags,
-            "state": getattr(item, "state", None)}
+        for item in sorted(list(items.all()) + list(scenes.all()), key=lambda i:i.name):
+            if not _include_item(item, include_tags, exclude_tags,
+                                 include_items, exclude_items):
+                continue
 
-        if isinstance(item, Number):
-            item_dict["inputs"] = [{"command": "set",
-                                    "type": "number"}]
-        elif isinstance(item, Toggle):
-            item_dict["inputs"] = [{"command": "on",
-                                    "type": "button"},
-                                   {"command": "off",
-                                    "type": "button"},
-                                   {"command": "toggle",
-                                    "type": "button"}]
-        elif isinstance(item, Trigger):
-            item_dict["inputs"] = [{"command": "trigger",
-                                    "type": "button"}]
-        elif isinstance(item, Motor):
-            item_dict["inputs"] = [{"command": "forward",
-                                    "type": "button"},
-                                   {"command": "reverse",
-                                    "type": "button"},
-                                   {"command": "stop",
-                                    "type": "button"}]
-        else:
-            item_dict["inputs"] = []
+            item_dict = {
+                "desc": item.name,
+                "name": utils.mangle_name(item.name),
+                "show_disable": "webui.show_disable" in item.tags,
+                "state": getattr(item, "state", getattr(item, "active", None)),
+                "disabled": not getattr(item, "enabled", False)
+            }
 
-        args["items"].append(item_dict)
+            if isinstance(item, Number):
+                item_dict["inputs"] = [{"command": "set",
+                                        "type": "number"}]
+            elif isinstance(item, Toggle):
+                item_dict["inputs"] = [{"command": "on",
+                                        "type": "button"},
+                                       {"command": "off",
+                                        "type": "button"},
+                                       {"command": "toggle",
+                                        "type": "button"}]
+            elif isinstance(item, Trigger):
+                item_dict["inputs"] = [{"command": "trigger",
+                                        "type": "button"}]
+            elif isinstance(item, Motor):
+                item_dict["inputs"] = [{"command": "forward",
+                                        "type": "button"},
+                                       {"command": "reverse",
+                                        "type": "button"},
+                                       {"command": "stop",
+                                        "type": "button"}]
+            elif isinstance(item, Scene):
+                item_dict["inputs"] = [{"type": "scene"}]
+            else:
+                item_dict["inputs"] = []
 
-    args["scenes"] = []
-    for scene in sorted(scenes.all(), key=lambda i:i.name):
-        scene_dict = {"desc": scene.name,
-                      "name": utils.mangle_name(scene.name),
-                      "active": bool(scene)
-        }
-        args["scenes"].append(scene_dict)
+            sect["item_list"].append(item_dict)
+        args["sections"].append(dict(sect))
 
     return env.get_template('main.html').render(**args)
 
@@ -122,6 +129,8 @@ def _main_js(*_, **__):
 
 def _include_item(item, include_tags, exclude_tags, include_items, exclude_items):
     return ((not include_tags or set(item.tags) & include_tags) or
-            (item.name in include_items)) and \
+            (item.name in include_items or
+             utils.mangle_name(item.name) in include_items)) and \
             (not exclude_tags or not set(item.tags) & exclude_tags) and \
-            (not exclude_items or not item.name in exclude_items)
+            (not exclude_items or (item.name not in exclude_items and
+                                   utils.mangle_name(item.name) not in exclude_items))
