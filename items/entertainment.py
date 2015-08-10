6f27f0a51614abc84adb38bf4adf6bf357150f99
Dylan Whichard <dylan@whichard.com>
2015-08-09 21:24:27 -0400

    Update entertainment room

--- items/entertainment.py
+++ items/entertainment.py
@@ -11,13 +11,13 @@ Toggle("Entertainment Room - Living Room Door",
        tags=("entertainment_room", "living_room", "door", "occupancy", "nyi"))
 
 Toggle("Entertainment Room - Laundry Room Door",
-       tags=("entertainment_room", "laundry_room", "door", "occupancy", "nyi"))
+       tags=("entertainment_room", "laundry_room", "door", "occupancy"))
 Toggle("Entertainment Room Motion",
-        tags=("entertainment_room", "motion", "occupancy", "nyi"))
+        tags=("entertainment_room", "motion", "occupancy"))
 
 Number("Entertainment Room Temperature",
-       #bindings={"http": {"pull": "CHANGEME:8081/temp"}},
-       tags=("entertainment_room", "temperature", "climate", "nyi"))
+       bindings={"http": {"pull": "vinyl:8081/temp"}},
+       tags=("entertainment_room", "temperature", "climate"))
 Number("Entertainment Room Humidity",
-       #bindings={"http": {"pull": "CHANGEME:8081/hum"}},
-       tags=("entertainment_room", "humidity", "climate", "nyi"))
+       bindings={"http": {"pull": "vinyl:8081/hum"}},
+       tags=("entertainment_room", "humidity", "climate"))
