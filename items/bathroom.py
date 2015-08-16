25417d2fc0836fe0312f841832187fa4f5cfd0c0
Dylan Whichard <dylan@whichard.com>
2015-08-15 22:24:45 -0400

    More Syntax

--- items/bathroom.py
+++ items/bathroom.py
@@ -7,4 +7,4 @@ Toggle("Bathroom Light",
 
 Toggle("Bathroom Door",
        tags=("bathroom", "door"),
-       bindings={"sign": ("on", {"message": "Bathroom Occupied", "effects": ["bounce_x"]}, "off")})
+       bindings={"sign": {"commands": ("on", {"message": "Bathroom Occupied", "effects": ["bounce_x"]}, "off")}})
