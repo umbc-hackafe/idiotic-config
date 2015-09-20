5705ce70b86e4bef2c6b193ca3d5960589a49b41
Dylan Whichard <dylan@whichard.com>
2015-09-20 00:28:50 -0400

    Add turning off sign for media mode

--- rules/media.py
+++ rules/media.py
@@ -2,18 +2,26 @@ from idiotic.rule import bind, Command, Change, Schedule, augment, Delay, DeDup
 from idiotic.scene import Scene
 from idiotic import items, scheduler, modules, scenes
 
+sign_placeholder = None
+
 class LivingRoomMedia(Scene):
     control = (items.living_room_lamp, items.kitchen_table_light)
     def entered(self):
+        global sign_placeholder
         items.living_room_projector.on(source='living_room_media')
+        sign_placeholder = modules.sign.new_message('', priority=60, name='placeolder')
         for item in self.control:
             item.off()
             item.disable()
 
     def exited(self):
+        global sign_placeolder
         for item in self.control:
             item.enable()
             item.on()
+        if sign_placeholder:
+            sign_placeholder.remove()
+            sign_placeholder = None
         items.living_room_projector.off(source='living_room_media')
 
 @bind(Change(items.living_room_projector))
