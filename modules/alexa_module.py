e1568e54c9a35f7e360fe90b1884494bc889f6d3
Dylan Whichard <dylan@whichard.com>
2015-10-15 01:54:57 -0400

    Wait until thing exists before doing thing

--- modules/alexa_module.py
+++ modules/alexa_module.py
@@ -1,5 +1,5 @@
 import logging
-import alexa
+import pyalexa
 from idiotic import dispatcher, event, items, scenes
 
 MODULE_NAME = "alexa"
@@ -14,52 +14,52 @@ def configure(config, api, assets):
     api.serve(SKILL.flask_target, '/', methods=['POST', 'GET'], raw_result=True)
 
 
-@SKILL.launch
-def launch(request):
-    return request.response(end=False)
+    @SKILL.launch
+    def launch(request):
+        return request.response(end=False)
 
-@SKILL.end
-def end(request):
-    return request.response(end=True)
+    @SKILL.end
+    def end(request):
+        return request.response(end=True)
 
-@SKILL.intent("SetSwitch")
-def set_switch(request):
-    if not request.data().get("Item"):
-        return request.response(end=False, speech="Please specify an item")
+    @SKILL.intent("SetSwitch")
+    def set_switch(request):
+        if not request.data().get("Item"):
+            return request.response(end=False, speech="Please specify an item")
 
-    if not request.data().get("NewStatus"):
-        return request.response(end=False, speech="Please specify a command")
+        if not request.data().get("NewStatus"):
+            return request.response(end=False, speech="Please specify a command")
 
-    name = request.data().get("Item")
-    command = request.data().get("NewStatus")
+        name = request.data().get("Item")
+        command = request.data().get("NewStatus")
 
-    try:
-        target = items[name]
-        target.command(command)
-        return request.response(end=True, speech="Okay.")
-    except NameError:
-        return request.response(end=False, speech="Item {} not found.".format(name))
-    except AttributeError:
-        return request.response(end=False, speech="Item {} has no command {}".format(name, command))
+        try:
+            target = items[name]
+            target.command(command)
+            return request.response(end=True, speech="Okay.")
+        except NameError:
+            return request.response(end=False, speech="Item {} not found.".format(name))
+        except AttributeError:
+            return request.response(end=False, speech="Item {} has no command {}".format(name, command))
 
-@SKILL.intent("SceneChange")
-def scene_change(request):
-    if not request.data().get("Scene"):
-        return request.response(end=False, speech="Please specify a scene.")
+    @SKILL.intent("SceneChange")
+    def scene_change(request):
+        if not request.data().get("Scene"):
+            return request.response(end=False, speech="Please specify a scene.")
 
-    if not request.data().get("Action"):
-        return request.response(end=False, speech="Please specify 'enter' or 'exit'")
+        if not request.data().get("Action"):
+            return request.response(end=False, speech="Please specify 'enter' or 'exit'")
 
-    name = request.data().get("Scene").lower().replace(' ', '')
-    action = request.data().get("Action").lower()
+        name = request.data().get("Scene").lower().replace(' ', '')
+        action = request.data().get("Action").lower()
 
-    try:
-        scene = scenes[name]
-        if action in ("enter", "start", "enable"):
-            scene.enter()
-            return request.response(end=True, speech="Entering scene {}".format(name))
-        elif action in ("exit", "stop", "disable"):
-            scene.exit()
-            return request.response(end=True, speech="Exiting scene {}".format(name))
-    except NameError:
-        return request.response("Scene '{}' not found".format(name))
+        try:
+            scene = scenes[name]
+            if action in ("enter", "start", "enable"):
+                scene.enter()
+                return request.response(end=True, speech="Entering scene {}".format(name))
+            elif action in ("exit", "stop", "disable"):
+                scene.exit()
+                return request.response(end=True, speech="Exiting scene {}".format(name))
+        except NameError:
+            return request.response("Scene '{}' not found".format(name))
