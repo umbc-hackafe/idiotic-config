48f3ee7a31b4c9feb2bffde839616c432a7a97fc
Dylan Whichard <dylan@whichard.com>
2015-10-18 23:27:31 -0400

    Fix fog machine

--- items/party.py
+++ items/party.py
@@ -7,16 +7,3 @@ Toggle("Fog Machine",
        bindings={"http": {"push": [
            ("on", "discord:8081/fog?state=0"),
            ("off", "discord:8081/fog?state=1")]}})
-
-Number("Living Room Temperature",
-       bindings={"http": {"pull": (60, "luna:8081/temp", None, float)}},
-       tags=("living_room", "temperature", "climate", "webui.show_sparkline"))
-Number("Living Room Humidity",
-       bindings={"http": {"pull": (60, "luna:8081/hum", None, float)}},
-       tags=("living_room", "humidity", "climate", "webui.show_sparkline",
-             "webui.enable_graph"))
-
-Scene("LivingRoomSleep",
-      active={"living_room_projector": False,
-              "living_room_lamp": (False, True),
-              "kitchen_table_light": (False, True)})
