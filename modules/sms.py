b638ecbfe2572efefc5e7c82cc49621cbeddc9a1
Dylan Whichard <dylan@whichard.com>
2015-11-02 16:48:41 -0500

    Implement SMS receiving

--- modules/sms.py
+++ modules/sms.py
@@ -2,6 +2,8 @@
 
 """
 
+import json
+import time
 import logging
 import requests
 import functools
@@ -14,12 +16,13 @@ log = logging.getLogger("module.sms")
 
 host = "localhost"
 
-class SMSEvent(event.BaseEvent):
-    def __init__(self, sender, recipient, body):
+class SMSReceivedEvent(event.BaseEvent):
+    def __init__(self, sender, recipient, body, time):
         super().__init__()
         self.sender = sender
         self.recipient = recipient
         self.body = body
+        self.timestamp = time
 
 def configure(config, api, assets):
     global host
@@ -30,10 +33,16 @@ def configure(config, api, assets):
         pass
 
     api.serve(receive, '/post_sms', methods=['POST'], raw_result=True,
-              no_source=True, content_type="application/json", get_data="json")
+              no_source=True, content_type="application/json", get_data="data")
 
 def send(recipient, body):
     pass
 
-def receive(*args, json=None, **kwargs):
-    log.debug(json)
+def receive(*args, data=None, **kwargs):
+    if data:
+        msg = json.loads(data.decode('UTF-8'))
+        evt = SMSReceivedEvent(msg.get("Message", {}).get("From", None),
+                       msg.get("Message", {}).get("To", None),
+                       msg.get("Message", {}).get("Body", ""),
+                       msg.get("Message", {}).get("Time", time.time()))
+        dispatcher.dispatch(evt)
