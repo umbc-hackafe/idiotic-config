c6bb448a4d58f4451638f7131d91ff3e4ddcf382
Dylan Whichard <dylan@whichard.com>
2015-11-02 16:35:46 -0500

    Add SMS module (test)

---
+++ modules/sms.py
@@ -0,0 +1,39 @@
+"""sms -- Receive and send SMS via Voipo
+
+"""
+
+import logging
+import requests
+import functools
+from collections import defaultdict
+from idiotic import dispatcher, event
+
+MODULE_NAME = "sms"
+
+log = logging.getLogger("module.sms")
+
+host = "localhost"
+
+class SMSEvent(event.BaseEvent):
+    def __init__(self, sender, recipient, body):
+        super().__init__()
+        self.sender = sender
+        self.recipient = recipient
+        self.body = body
+
+def configure(config, api, assets):
+    global host
+    if "host" in config:
+        host = config["host"]
+
+    if "username" in config and "password" in config:
+        pass
+
+    api.serve(receive, '/post_sms', methods=['POST'], raw_result=True,
+              no_source=True, content_type="application/json", get_data="json")
+
+def send(recipient, body):
+    pass
+
+def receive(*args, json=None, **kwargs):
+    print(json)
