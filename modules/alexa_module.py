5f4435c02814ecaf89a8b3a21b58cafb9732140b
Dylan Whichard <dylan@whichard.com>
2015-11-03 20:06:38 -0500

    Add example phrases generator for alexa skill

--- modules/alexa_module.py
+++ modules/alexa_module.py
@@ -1,5 +1,6 @@
 import logging
 import pyalexa
+import itertools
 from idiotic import dispatcher, event, items, scenes
 
 MODULE_NAME = "alexa"
@@ -7,13 +8,16 @@ VERSION = "2"
 
 SKILL = None
 
+SCENE_ENTER_COMMANDS = ("enter", "start", "enable")
+SCENE_EXIT_COMMANDS = ("exit", "stop", "disable", "leave")
+
 def configure(config, api, assets):
     global SKILL
     SKILL = pyalexa.Skill(app_id=config["app_id"])
 
     api.serve(SKILL.flask_target, '/', methods=['POST', 'GET'], raw_result=True,
               no_source=True, content_type="application/json")
-
+    api.serve(generate_utterances, '/utterances', raw_result=True, no_source=True, content_type="text/plain")
 
     @SKILL.launch
     def launch(request):
@@ -69,11 +73,72 @@ def configure(config, api, assets):
 
         try:
             scene = scenes[name]
-            if action in ("enter", "start", "enable"):
+            if action in SCENE_ENTER_COMMANDS:
                 scene.enter()
                 return request.response(end=True, speech="Entering scene {}".format(name))
-            elif action in ("exit", "stop", "disable"):
+            elif action in SCENE_EXIT_COMMANDS:
                 scene.exit()
                 return request.response(end=True, speech="Exiting scene {}".format(name))
         except NameError:
             return request.response("Scene '{}' not found".format(name))
+
+UTTERANCES = [
+    (
+        "GetState", [
+            ("Item", items.all)
+        ], [
+            "about the {{{0.name}|Item}}"
+        ]
+    ),(
+        "SetSwitch",
+        [
+            ("Item", items.all),
+            ("NewStatus", lambda i: i.commands())
+        ], [
+            "Turn {{{0.name}|Item}} {{{1}|NewStatus}}",
+            "Turn the {{{0.name}|Item}} {{{1}|NewStatus}}",
+            "Turn {{{1}|NewStatus}} {{{0.name}|Item}}",
+            "Turn {{{1}|NewStatus}} the {{{0.name}|Item}}",
+            "Set {{{0.name}|Item}} {{{1}|NewStatus}}",
+            "Set the {{{0.name}|Item}} {{{1}|NewStatus}}",
+            "Set {{{1}|NewStatus}} {{{0.name}|Item}}",
+            "Set {{{1}|NewStatus}} the {{{0.name}|Item}}",
+        ]
+    ),(
+        "SceneChange",
+        [
+            ("Scene", scenes.all),
+            ("Action", lambda _: SCENE_ENTER_COMMANDS + SCENE_EXIT_COMMANDS)
+        ], [
+            "{{{1}|Action}} {{{0.name}|Scene}}",
+            "{{{1}|Action}} the {{{0.name}|Scene}}",
+            "{{{1}|Action}} the {{{0.name}|Scene}} scene",
+            "{{{1}|Action}} the scene {{{0.name}|Scene}}",
+            "{{{1}|Action}} scene {{{0.name}|Scene}}",
+        ]
+    ),
+]
+
+def __product(*args):
+    return itertools.product(*(arg if isinstance(arg, (list,tuple)) else [arg] for arg in args))
+
+def __parse_utterance(name, slots, phrases, items=[]):
+    if not slots:
+        return [name + " " + phrase.format(*slotvals)
+                for phrase in phrases
+                for item in items
+                for slotvals in __product(*item)]
+
+    slot_name, item_lambda = slots[0]
+    slots = slots[1:]
+
+    if items:
+        for i, v in enumerate(items):
+            items[i] = v + (item_lambda(*v),)
+    else:
+        items = list(zip(item_lambda()))
+
+    return __parse_utterance(name, slots, phrases, items)
+
+def generate_utterances():
+    return '\n'.join(('\n'.join(__parse_utterance(*utterance)) for utterance in UTTERANCES))
