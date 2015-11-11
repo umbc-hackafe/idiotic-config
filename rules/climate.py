b6793bdbbbffe07e92c7d9fd43653d202b2bbc8d
Dylan Whichard <dylan@whichard.com>
2015-11-10 21:46:00 -0500

    Fix time vs datetime

--- rules/climate.py
+++ rules/climate.py
@@ -3,19 +3,19 @@ from idiotic.item import Group
 from idiotic.scene import Scene
 from idiotic import items, scheduler, modules, scenes
 
-import time
+import datetime
 
 def avg(l):
     return sum(l)/len(l)
 
 Group("Average Temperature",
       tags=("temperature",),
-      state=lambda ms: avg([m.state for m in ms if m.state and (time.time() - m.state_history.last().time) < 1800]),
+      state=lambda ms: avg([m.state for m in ms if m.state and (datetime.datetime.now() - m.state_history.last().time).total_seconds() < 1800]),
       members=[i for i in items.all() if "temperature" in i.tags and "nyi" not in i.tags])
 
 Group("Average Humidity",
       tags=("humidity",),
-      state=lambda ms: avg([m.state for m in ms if m.state and (time.time() - m.state_history.last().time) < 1800]),
+      state=lambda ms: avg([m.state for m in ms if m.state and (datetime.datetime.now() - m.state_history.last().time).total_seconds() < 1800]),
       members=[i for i in items.all() if "humidity" in i.tags and "nyi" not in i.tags])
 
 # Prevent furnace from turning on and of too quickly
