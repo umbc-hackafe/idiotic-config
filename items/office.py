b62ea2ed1e56b0d5b23e1475a2daec479ec38eed
Dylan Whichard <dylan@whichard.com>
2015-11-29 22:37:46 -0500

    Update office for new items

--- items/office.py
+++ items/office.py
@@ -2,21 +2,22 @@ from idiotic.item import Toggle, Trigger, Number, Group
 from idiotic import modules, scheduler
 
 Toggle("Office Light",
-       tags=("office", "light", "nyi"))
+       bindings={"x10": {"code": "b4"},
+       tags=("office", "light"))
 
 Toggle("Office Motion",
-        tags=("office", "motion", "occupancy", "nyi"))
+        tags=("office", "motion", "occupancy"))
 
 Toggle("Office Door",
        tags=("office", "hallway", "door", "occupancy", "nyi"))
 
 Number("Office Temperature",
-       tags=("office", "temperature", "climate", "nyi"))
+       bindings={"http": {"pull": "pinkie:8081/temp"}},
+       tags=("office", "temperature", "climate"))
 
 Number("Office Humidity",
-       #bindings={"http": {"pull": "dash:8081/temp"}},
-       tags=("office", "humidity", "climate", "nyi"))
+       bindings={"http": {"pull": "pinkie:8081/hum"}},
+       tags=("office", "humidity", "climate"))
 
 Toggle("Office Air Conditioner",
-       #bindings={"http": {"pull": "dash:8081/hum"}},
        tags=("office", "ac", "climate", "nyi"))
