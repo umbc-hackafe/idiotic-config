d124e0e7f309f48019acc8160781e4f2624cb11d
Dylan Whichard <dylan@whichard.com>
2016-04-06 00:51:23 -0400

    Asyncio-ify network module

--- modules/network.py
+++ modules/network.py
@@ -1,42 +1,37 @@
 """network -- Ping a device to see if it is on the network
 """
 from idiotic.timer import Timer
-import logging
 import subprocess
+import logging
+import asyncio
 import time
 import os
-import threading
 
 LOG = logging.getLogger("module.network")
 
 MODULE_NAME = "network"
 
 checks = []
-checkTimer = None
-refreshThread = None
 
-def _refresh():
-    global refreshThread
-    refreshThread = threading.Thread(target=__refresh)
-    refreshThread.isDaemon = True
-    refreshThread.start()
+def start():
+    scheduler.every().second.do(_refresh)
 
-def __refresh():
-    global checkTimer
+def _refresh(now=False):
     ctime = int(time.time())
     for check in checks:
-        if (ctime % check['interval']) == 0:
+        if now or ((ctime % check['interval']) == 0):
             if check['action'] == 'ping':
                 for host in check['hosts']:
                     with open(os.devnull, 'w') as FNULL:
-                        if not subprocess.call(["ping", "-c", "3", "-i", "0.01", host], stdout=FNULL, stderr=subprocess.STDOUT):
+                        process = yield from asyncio.create_subprocess_exec("/usr/bin/ping","-c", "3", "-i", "0.2", host, stdout=FNULL, stderr=subprocess.STDOUT)
+                        _ = yield from process.communicate()
+                        if process.returncode == 0:
                             check['item'].on()
                             break
                 else:
                     check['item'].off()
             else:
                 LOG.info("Unknown action {action}".format(action=action))
-    checkTimer.reschedule()
 
 def bind_item(item, action="ping", hosts=["localhost"], interval=60):
     LOG.info("Binding {name} to ping".format(name=item.name))
@@ -47,9 +42,3 @@ def bind_item(item, action="ping", hosts=["localhost"], interval=60):
             "item": item,
         },
     )
-
-    global checkTimer
-    if not checkTimer:
-        LOG.info("Starting network update timer")
-        checkTimer = Timer(1, _refresh)
-        _refresh()
