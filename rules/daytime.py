9100bc36eb2ddc41b6e81f93507732f8102e25b2
Dylan Whichard <dylan@whichard.com>
2016-04-10 03:18:48 -0400

    Fix daytime

--- rules/daytime.py
+++ rules/daytime.py
@@ -1,5 +1,8 @@
 from idiotic.rule import bind, Schedule
+from idiotic.scene import Scene
 from idiotic import instance as c
+
+Scene("Daytime")
 
 @bind(Schedule(c.scheduler.every().day.at("8:00")))
 def enter_sun_mode(evt):
